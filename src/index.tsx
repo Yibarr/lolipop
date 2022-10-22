aa5b08508bb40325d52953bc2945afb46c798206
Yael Ibarra <yaelibarra@Yaels-MacBook-Pro.local>
2022-10-21 22:53:22 -0500

    Project structure implemented to react app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Presentation/index.css';
+import App from './Presentation/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
