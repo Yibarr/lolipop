5412a91002f79d9a308f089cbc98a7d66aef21a7
Luis Salgado <luis_salgado1999@outlook.com>
2022-11-04 15:01:43 -0600

    Styles for the principal page

--- src/Presentation/App.tsx
+++ src/Presentation/App.tsx
@@ -1,48 +1,41 @@
-import React from 'react';
-import logo from '../Resources/logo.svg';
-import './App.css';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
+import React from "react";
+import "./App.css";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
+
+const paperStyle = {
+  p: "2px 4px",
+  display: "flex",
+  alignItems: "center",
+  width: 300,
+  heigth: 300,
+  justifyContent: "center",
+  
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="pond">
+      <div className="froggy-1">
         <Paper
           component="form"
-          sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-        >
-          <IconButton sx={{ p: '10px' }} aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+          sx={paperStyle}
+        >         
           <InputBase
             sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Google Maps"
-            inputProps={{ 'aria-label': 'search google maps' }}
+            placeholder="Summoner"
+            inputProps={{ "aria-label": "search google maps" }}
           />
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
             <SearchIcon />
           </IconButton>
-          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-          <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-            <DirectionsIcon />
-          </IconButton>
         </Paper>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
