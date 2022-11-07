cbfed6a3ef828c5d5a90b34d2b97ac4e003037e3
parzzivaal <53958297+parzzivaal@users.noreply.github.com>
2022-11-07 16:35:52 -0600

    Navbar is added

--- src/Presentation/Pages/Home/Home.tsx
+++ src/Presentation/Pages/Home/Home.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
-import './App.css';
+import Navbar from "../../Components/Navbar/Navbar";
+import './Home.css';
 
-const paperStyle = {
-  p: '2px 4px',
-  display: 'flex',
-  alignItems: 'center',
-  width: 300,
-  heigth: 300,
-  justifyContent: 'center',
-  
-};
-
-function App() {
-  return (
-    <div className='pond'>
-      <div className='froggy-1'>
+function Home() {
+  return(
+    <div >
+      <Navbar/>
+      
+      <div className = 'Home_Container' >
         <Paper
           component='form'
-          sx={paperStyle}
+          sx={{
+            p: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: 300,
+            heigth: 300,
+            justifyContent: 'center'
+          }}
         >         
           <InputBase
             sx={{ ml: 1, flex: 1 }}
@@ -35,9 +32,11 @@ function App() {
             <SearchIcon />
           </IconButton>
         </Paper>
+        
       </div>
     </div>
   );
+  
 }
 
-export default App;
+export default Home;
