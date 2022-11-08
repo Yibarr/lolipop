c37823f2d52a13f81bbebe3194c0afdde32cc47d
Luis Salgado <luis_salgado1999@outlook.com>
2022-11-07 18:58:36 -0600

    Navegador

--- src/Presentation/App.tsx
+++ src/Presentation/App.tsx
@@ -1,37 +1,106 @@
-import React from 'react';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
-import './App.css';
+import React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import "./App.css";
 
 const paperStyle = {
-  p: '2px 4px',
-  display: 'flex',
-  alignItems: 'center',
+  p: "2px 4px",
+  display: "flex",
+  alignItems: "center",
   width: 300,
   heigth: 300,
-  justifyContent: 'center',
-  
+  justifyContent: "center",
 };
 
+const pages = ["Home", "Items"];
+
 function App() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div className='pond'>
-      <div className='froggy-1'>
-        <Paper
-          component='form'
-          sx={paperStyle}
-        >         
+    <div className="pond">
+      <div className="froggy1">
+        <AppBar>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 3,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "-moz-initial",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 13 }} />
+
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{
+                      color: "black",
+                      display: "-ms-grid",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        );
+      </div>
+
+      <div className="froggy-2">
+        <Paper component="form" sx={paperStyle}>
           <InputBase
             sx={{ ml: 1, flex: 1 }}
-            placeholder='Summoner'
-            inputProps={{ 'aria-label': 'search google maps' }}
+            placeholder="Summoner"
+            inputProps={{ "aria-label": "search google maps" }}
           />
-          <IconButton type='button' sx={{ p: '10px' }} aria-label='search'>
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
             <SearchIcon />
           </IconButton>
         </Paper>
