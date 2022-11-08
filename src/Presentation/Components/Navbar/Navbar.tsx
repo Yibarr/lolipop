5a9972c16beb9f541227e1655c1821e1547083b5
Luis Salgado <luis_salgado1999@outlook.com>
2022-11-07 18:58:36 -0600

    Navbar component is added for home page

---
+++ src/Presentation/Components/Navbar/Navbar.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+function Navbar() {
+    return (
+    
+      <div className = "navbar">
+      <React.Fragment>
+             <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+          <CssBaseline />
+              <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Lolipop
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Builds
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Invocador
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Support
+            </Link>
+            </nav>
+          
+           </Toolbar>
+           </AppBar>
+      
+     
+        </React.Fragment>
+      </div>
+      );
+}      
+      export default Navbar;
---
+++ src/Presentation/Pages/Home/Home.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Navbar from "../../Components/Navbar/Navbar";
+import './Home.css';
+
+function Home() {
+  return(
+    <div >
+      <Navbar/>
+      
+      <div className = 'Home_Container' >
+        <Paper
+          component='form'
+          sx={{
+            p: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: 300,
+            heigth: 300,
+            justifyContent: 'center'
+          }}
+        >         
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Summoner"
+            inputProps={{ "aria-label": "search google maps" }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        
+      </div>
+    </div>
+  );
+  
+}
+
+export default Home;
